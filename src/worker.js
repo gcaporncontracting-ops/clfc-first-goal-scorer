a043f487e426b6b0d573198e6c45036faeee437c
gcaporncontracting-ops <3.07902086e+08+gcaporncontracting-ops@users.noreply.github.com>
2026-08-18 05:56:16 +0000

    Update worker.js with latest changes

--- src/worker.js
+++ src/worker.js
@@ -214,7 +214,7 @@ var ENTRY_FEE = 5;
 var PAYID_EMAIL = "playersfund@clfc.com";
 var EXPECTED_PLAYERS = 22;
 var TESTING_MASTER_PIN = "0000";
-var ADMIN_PASSCODE = "94172079";
+var ADMIN_PASSCODE = "Warriors-YE8899UE";
 function json(data, status = 200) {
   return new Response(JSON.stringify(data), {
     status,
